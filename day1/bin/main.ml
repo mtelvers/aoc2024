6f56160968eadd9fb754a44f1046e858d7ff45ef
Mark Elvers <mark.elvers@tunbury.org>
2024-12-01 11:49:16 +0000

    Day 1 - part two

--- day1/bin/main.ml
+++ day1/bin/main.ml
@@ -4,18 +4,22 @@ let read_input =
     try
       let line = input_line input in
       loop input (line :: lines)
-    with End_of_file ->
-      close_in input;
-      lines
+    with
+    | End_of_file ->
+        close_in input;
+        lines
   in
   loop ic []
 
-let lst1, lst2 = List.fold_left (fun lst line ->
-    (Scanf.sscanf line " %i %i " (fun i j -> (i, j))) :: lst
-  ) [] read_input |> List.split
-
-let sum = List.fold_left2 (fun sum i j -> sum + Int.abs (i - j)) 0 (List.sort compare lst1) (List.sort compare lst2)
-
+let left, right = List.fold_left (fun lst line -> Scanf.sscanf line " %i %i " (fun l r -> (l, r)) :: lst) [] read_input |> List.split
+let sum = List.fold_left2 (fun sum l r -> sum + Int.abs (l - r)) 0 (List.sort compare left) (List.sort compare right)
 let () = Printf.printf "part 1: %i\n" sum
+
+let sum =
+  List.fold_left
+    (fun sum l ->
+      let count = List.fold_left (fun n r -> if l = r then n + 1 else n) 0 right in
+      sum + (l * count))
+    0 left
 
 let () = Printf.printf "part 2: %i\n" sum
