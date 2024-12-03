3524de91ed274e5ef1def0ffdf3738c018ed251a
Mark Elvers <mark.elvers@tunbury.org>
2024-12-03 09:04:52 +0000

    Day 3 - part two

--- day3/bin/main.ml
+++ day3/bin/main.ml
@@ -11,11 +11,36 @@ end
 let part1 =
   let r = Str.regexp {|mul([0-9]+,[0-9]+)|} in
   Str.full_split r input
-  |> List.filter_map (function
-       | Str.Text _ -> None
-       | Delim x ->
-           let r = Str.regexp {|[(,)]|} in
-           Some (Str.split r x |> List.filter_map Int.of_string_opt |> List.fold_left (fun sum x -> sum * x) 1))
-  |> List.fold_left (fun t x -> t + x) 0
+  |> List.fold_left
+       (fun acc s ->
+         match s with
+         | Str.Text _ -> acc
+         | Delim x ->
+             let r = Str.regexp {|[(,)]|} in
+             let mul = Str.split r x |> List.filter_map Int.of_string_opt |> List.fold_left (fun sum x -> sum * x) 1 in
+             acc + mul)
+       0
 
 let () = Printf.printf "part 1: %i\n" part1
+
+type t = {
+  add : int;
+  total : int;
+}
+
+let part2 =
+  let r = Str.regexp {|mul([0-9]+,[0-9]+)\|do()\|don't()|} in
+  Str.full_split r input
+  |> List.fold_left
+       (fun acc s ->
+         match s with
+         | Str.Text _ -> acc
+         | Delim "do()" -> { acc with add = 1 }
+         | Delim "don't()" -> { acc with add = 0 }
+         | Delim x ->
+             let r = Str.regexp {|[(,)]|} in
+             let mul = Str.split r x |> List.filter_map Int.of_string_opt |> List.fold_left (fun sum x -> sum * x) 1 in
+             { acc with total = acc.total + (acc.add * mul) })
+       { add = 1; total = 0 }
+
+let () = Printf.printf "part 2: %i\n" part2.total
