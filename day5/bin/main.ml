3c4be327a3320d98823ade644d9d935080118459
Mark Elvers <mark.elvers@tunbury.org>
2024-12-05 08:42:49 +0000

    Day 5 - part two

--- day5/bin/main.ml
+++ day5/bin/main.ml
@@ -7,26 +7,30 @@ let rules, pages =
          try String.index s '|' > 0 with
          | Not_found -> false)
 
+let rules =
+  List.map
+    (fun r ->
+      r |> String.split_on_char '|' |> List.map int_of_string |> function
+      | [ p1; p2 ] -> (p1, p2)
+      | _ -> assert false)
+    rules
+
 let pages = List.map (fun p -> p |> String.split_on_char ',' |> List.map int_of_string) pages
+let rcompare a b = if List.mem (a, b) rules then -1 else if List.mem (b, a) rules then 1 else 0
 
 let rec test = function
   | [] -> true
-  | p1 :: tl ->
-      List.fold_left
-        (fun acc p2 ->
-          let p = Printf.sprintf "%i|%i" p1 p2 in
-          let pr = Printf.sprintf "%i|%i" p2 p1 in
-          acc && if List.mem p rules then true else if List.mem pr rules then false else assert false)
-        true tl
-      && test tl
+  | p1 :: tl -> List.fold_left (fun acc p2 -> acc && rcompare p1 p2 = -1) true tl && test tl
 
-let part1 =
+let part1, part2 =
   List.fold_left
-    (fun sum l ->
-      if test l then
-        let mid = List.length l / 2 in
-        sum + List.nth l mid
-      else sum)
-    0 pages
+    (fun (sum1, sum2) l ->
+      let mid = List.length l / 2 in
+      if test l then (sum1 + List.nth l mid, sum2)
+      else
+        let nl = List.sort rcompare l in
+        (sum1, sum2 + List.nth nl mid))
+    (0, 0) pages
 
 let () = Printf.printf "part 1: %i\n" part1
+let () = Printf.printf "part 2: %i\n" part2
