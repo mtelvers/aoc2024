d9c2da4d45cfef6e23e8070f24ddeb032fb1e423
Mark Elvers <mark.elvers@tunbury.org>
2024-12-06 10:43:15 +0000

    Only check the path

--- day6/bin/main.ml
+++ day6/bin/main.ml
@@ -15,7 +15,6 @@ let lab =
   input |> List.mapi (fun y line -> List.init (String.length line) (String.get line) |> List.mapi (fun x ch -> ({ y; x }, ch))) |> List.flatten |> Lab.of_list
 
 let start, _ = lab |> Lab.filter (fun _ ch -> ch = '^') |> Lab.choose
-
 let card = Lab.cardinal lab
 
 let rec walk pos dir lab limit =
@@ -30,18 +29,16 @@ let rec walk pos dir lab limit =
         new_pos :: walk new_pos dir lab (limit - 1)
     | _ -> []
 
-let part1 = walk start { y = -1; x = 0 } lab card |> List.sort_uniq compare |> List.length
+let path = walk start { y = -1; x = 0 } lab card |> List.sort_uniq compare
+let part1 = path |> List.length
 let () = Printf.printf "part 1: %i\n" part1
 
 let part2 =
-  Lab.fold
-    (fun p ch sum ->
-      match ch with
-      | '.' ->
-          let new_lab = Lab.add p '#' lab in
-          let len = walk start { y = -1; x = 0 } new_lab card |> List.length in
-          sum + if len = card then 1 else 0
-      | _ -> sum)
-    lab 0
+  List.fold_left
+    (fun sum p ->
+      let new_lab = Lab.add p '#' lab in
+      let len = walk start { y = -1; x = 0 } new_lab card |> List.length in
+      sum + if len = card then 1 else 0)
+    0 path
 
 let () = Printf.printf "part 2: %i\n" part2
