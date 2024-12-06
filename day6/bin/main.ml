78c236c992460af597861cd417ffe5b99d640b45
Mark Elvers <mark.elvers@tunbury.org>
2024-12-06 11:22:30 +0000

    Use a Map to hold the traversed path
    I wasn't happy with the assumption that paths longer than the area of
    the room should be discarded. However, since for part 1 I didn't bother
    keeping track of the direction the guard was moving this was an easy way
    to solve part 2. The code ran in ~10 seconds though which was acceptable.
    
    This version uses a Map to hold the list of the directions the guard
    was moving on each square. Therefore, loops can be detected as soon as
    they occur.  Disappointingly slow at nearly 16 seconds.

--- day6/bin/main.ml
+++ day6/bin/main.ml
@@ -5,40 +5,44 @@ type coord = {
   x : int;
 }
 
-module Lab = Map.Make (struct
+module CoordMap = Map.Make (struct
   type t = coord
 
   let compare = compare
 end)
 
 let lab =
-  input |> List.mapi (fun y line -> List.init (String.length line) (String.get line) |> List.mapi (fun x ch -> ({ y; x }, ch))) |> List.flatten |> Lab.of_list
+  input
+  |> List.mapi (fun y line -> List.init (String.length line) (String.get line) |> List.mapi (fun x ch -> ({ y; x }, ch)))
+  |> List.flatten |> CoordMap.of_list
 
-let start, _ = lab |> Lab.filter (fun _ ch -> ch = '^') |> Lab.choose
-let card = Lab.cardinal lab
+let start, _ = lab |> CoordMap.filter (fun _ ch -> ch = '^') |> CoordMap.choose
 
-let rec walk pos dir lab limit =
-  if limit = 0 then []
-  else
-    let new_pos = { y = pos.y + dir.y; x = pos.x + dir.x } in
-    match Lab.find_opt new_pos lab with
-    | None -> []
-    | Some '#' -> walk pos { y = dir.x; x = -dir.y } lab limit
-    | Some '^'
-    | Some '.' ->
-        new_pos :: walk new_pos dir lab (limit - 1)
-    | _ -> []
+let rec walk lab current dir path =
+  let path = CoordMap.add_to_list current dir path in
+  let new_pos = { y = current.y + dir.y; x = current.x + dir.x } in
+  match CoordMap.find_opt new_pos lab with
+  | None -> (false, path)
+  | Some '#' -> walk lab current { y = dir.x; x = -dir.y } path
+  | Some '^'
+  | Some '.' -> (
+      match CoordMap.find_opt new_pos path with
+      | Some lst when List.mem dir lst -> (true, path)
+      | Some _
+      | None ->
+          walk lab new_pos dir path)
+  | _ -> (false, path)
 
-let path = walk start { y = -1; x = 0 } lab card |> List.sort_uniq compare
-let part1 = path |> List.length
+let _, path = walk lab start { y = -1; x = 0 } CoordMap.empty
+let part1 = CoordMap.cardinal path
 let () = Printf.printf "part 1: %i\n" part1
 
 let part2 =
-  List.fold_left
-    (fun sum p ->
-      let new_lab = Lab.add p '#' lab in
-      let len = walk start { y = -1; x = 0 } new_lab card |> List.length in
-      sum + if len = card then 1 else 0)
-    0 path
+  CoordMap.fold
+    (fun pos _ sum ->
+      let new_lab = CoordMap.add pos '#' lab in
+      let loop, _ = walk new_lab start { y = -1; x = 0 } CoordMap.empty in
+      sum + if loop then 1 else 0)
+    path 0
 
 let () = Printf.printf "part 2: %i\n" part2
