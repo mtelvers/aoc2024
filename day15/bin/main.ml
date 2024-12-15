b7e7af4f3bdf3229c690494223c7aef7faff957e
Mark Elvers <mark.elvers@tunbury.org>
2024-12-15 11:56:53 +0000

    Tidy up of part 1

--- day15/bin/main.ml
+++ day15/bin/main.ml
@@ -11,7 +11,13 @@ module CoordMap = Map.Make (struct
   let compare = compare
 end)
 
-let map, directions = input |> List.filter (fun line -> String.length line > 0) |> List.partition (fun line -> match String.index_opt line '#' with | Some _ -> true | None -> false)
+let map, directions =
+  input
+  |> List.filter (fun line -> String.length line > 0)
+  |> List.partition (fun line ->
+         match String.index_opt line '#' with
+         | Some _ -> true
+         | None -> false)
 
 let warehouse =
   map
@@ -19,66 +25,54 @@ let warehouse =
   |> List.flatten |> CoordMap.of_list
 
 let print w =
-  CoordMap.fold (fun {y;_} ch ly ->
+  CoordMap.fold
+    (fun { y; _ } ch ly ->
       let () = if not (ly = y) then Printf.printf "\n" in
       let () = Printf.printf "%c" ch in
-      y
-    ) w 0
+      y)
+    w 0
 
 let directions = directions |> String.concat "" |> fun s -> List.init (String.length s) (String.get s)
 let _ = print warehouse
-
 let () = List.iter (Printf.printf "%c") directions
-
 
 let rec run wh r = function
   | [] -> wh
-  | d :: tl ->
-let () = Printf.printf "robot at %i,%i\n" r.x r.y in
-let direction = match d with
-  | '<' -> { x = -1; y = 0 }
-  | '>' -> { x = 1; y = 0 }
-  | '^' -> { x = 0; y = -1 }
-  | 'v' -> { x = 0; y = 1 }
-  | _ -> assert false in
+  | d :: tl -> (
+      let () = Printf.printf "robot at %i,%i\n" r.x r.y in
+      let _ = print wh in
+      let direction =
+        match d with
+        | '<' -> { x = -1; y = 0 }
+        | '>' -> { x = 1; y = 0 }
+        | '^' -> { x = 0; y = -1 }
+        | 'v' -> { x = 0; y = 1 }
+        | _ -> assert false
+      in
+      let rec loop push (p, ch) =
+        let np = { y = p.y + direction.y; x = p.x + direction.x } in
+        match CoordMap.find_opt np wh with
+        | Some '.' -> CoordMap.add np ch push
+        | Some 'O' -> loop (CoordMap.add np ch push) (np, 'O')
+        | Some '#' -> CoordMap.empty
+        | Some _
+        | None ->
+            assert false
+      in
+      let moves = loop CoordMap.(empty |> add r '.') (r, '@') in
+      match CoordMap.is_empty moves with
+      | true -> run wh r tl
+      | false -> run (CoordMap.union (fun _ _ b -> Some b) wh moves) { y = r.y + direction.y; x = r.x + direction.x } tl)
 
-let rec loop p w =
-  let np = { y = p.y + direction.y; x = p.x + direction.x; } in
-  match CoordMap.find_opt np w with
-  | Some '.' -> ['.']
-  | Some 'O' -> loop np w @ ['O']
-  | Some '#' -> ['#']
-  | Some _
-  | None -> assert false in
-
-let moves = (loop r wh) @ ['@'] in
-
-match moves with
-| []
-| '#' :: _  -> run wh r tl
-| hd :: tl2 ->
-  let moves = hd :: List.rev tl2 in
-
-let () = List.iter (fun ch ->
-Printf.printf "move %c\n" ch
-    ) moves in
-
-let wh, _ = List.fold_left (fun (w, p) ch -> 
-  (CoordMap.add p ch w,
-  { y = p.y + direction.y; x = p.x + direction.x; })
-        ) (wh, r) moves in
-
-let _ = print wh in
-  run wh { y = r.y + direction.y; x = r.x + direction.x} tl
-
-let robot = CoordMap.filter (fun _ ch -> ch = '@') warehouse |> CoordMap.choose |> fun (p, _)  -> p
-
+let robot = CoordMap.filter (fun _ ch -> ch = '@') warehouse |> CoordMap.choose |> fun (p, _) -> p
 let warehouse = run warehouse robot directions
 
-let gps = CoordMap.fold (fun p c sum ->
-    match c with
-    | 'O' -> sum + 100 * p.y + p.x
-    | _ -> sum
-  ) warehouse 0
+let gps =
+  CoordMap.fold
+    (fun p c sum ->
+      match c with
+      | 'O' -> sum + (100 * p.y) + p.x
+      | _ -> sum)
+    warehouse 0
 
 let () = Printf.printf "part 1: %i\n" gps
