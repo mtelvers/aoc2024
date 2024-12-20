55b8081c4c833b21a3834ecac49ecca715b1500b
Mark Elvers <mark.elvers@tunbury.org>
2024-12-20 13:46:39 +0000

    Day 20 - part two

--- day20/bin/main.ml
+++ day20/bin/main.ml
@@ -20,7 +20,7 @@ let start = track |> CoordMap.filter (fun _ ch -> ch = 'S') |> CoordMap.choose |
 let finish = track |> CoordMap.filter (fun _ ch -> ch = 'E') |> CoordMap.choose |> fun (finish, _) -> finish
 let cheats = Hashtbl.create 10000
 
-let rec loop depth track visited cheat =
+let rec loop depth track visited cheat range =
   let options = CoordMap.filter (fun _ d -> d = depth) visited in
   let visited =
     CoordMap.fold
@@ -42,38 +42,30 @@ let rec loop depth track visited cheat =
                  on_track && not (CoordMap.mem p visited))
         in
         let () =
-          [
-            { x = pos.x + 2; y = pos.y };
-            { x = pos.x - 2; y = pos.y };
-            { x = pos.x; y = pos.y - 2 };
-            { x = pos.x; y = pos.y + 2 };
-            { x = pos.x + 1; y = pos.y + 1 };
-            { x = pos.x - 1; y = pos.y - 1 };
-            { x = pos.x + 1; y = pos.y - 1 };
-            { x = pos.x - 1; y = pos.y + 1 };
-          ]
-          |> List.iter (fun p ->
-                 match CoordMap.find_opt p cheat with
-                 | Some v -> Hashtbl.add cheats (pos, p) (d + v)
-                 | None -> ())
+          for x = -range to range do
+            for y = -range to range do
+              if abs x + abs y <= range then
+                let p = { x = pos.x + x; y = pos.y + y } in
+                match CoordMap.find_opt p cheat with
+                | Some v -> Hashtbl.add cheats (pos, p) (d + v + abs x + abs y)
+                | None -> ()
+            done
+          done
         in
         List.fold_left (fun acc p -> CoordMap.add p (d + 1) acc) visited options)
       options visited
   in
   match CoordMap.find_opt finish visited with
-  | None -> loop (depth + 1) track visited cheat
+  | None -> loop (depth + 1) track visited cheat range
   | Some _ -> visited
 
-let without_cheating = loop 0 track CoordMap.(empty |> add start 0) CoordMap.empty
-let best = CoordMap.cardinal without_cheating - 1
-let steps_remaining = CoordMap.map (fun v -> 1 + best - v) without_cheating
-let _ = loop 0 track CoordMap.(empty |> add start 0) steps_remaining
-
-let part1 =
-  Hashtbl.fold
-    (fun (start, finish) v acc ->
-      let () = Printf.printf "(%i,%i) -> (%i,%i) = %i\n" start.x start.y finish.x finish.y v in
-      if best - v >= 100 then acc + 1 else acc)
-    cheats 0
-
+let without_cheating = loop 0 track CoordMap.(empty |> add start 0) CoordMap.empty 0
+let best = CoordMap.cardinal without_cheating - 1 (* remove initial '0' step *)
+let steps_remaining = CoordMap.map (fun v -> best - v) without_cheating
+let _ = loop 0 track CoordMap.(empty |> add start 0) steps_remaining 2
+let part1 = Hashtbl.fold (fun _ v acc -> if best - v >= 100 then acc + 1 else acc) cheats 0
 let () = Printf.printf "part 1: %i\n" part1
+let () = Hashtbl.clear cheats
+let _ = loop 0 track CoordMap.(empty |> add start 0) steps_remaining 20
+let part2 = Hashtbl.fold (fun _ v acc -> if best - v >= 100 then acc + 1 else acc) cheats 0
+let () = Printf.printf "part 2: %i\n" part2
