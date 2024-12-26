ee48a1e3b170cf5a62a2afe6fc8d022637ee73b2
Mark Elvers <mark.elvers@tunbury.org>
2024-12-26 14:28:43 +0000

    Day 21 - part one

--- day21/bin/main.ml
+++ day21/bin/main.ml
@@ -5,50 +5,48 @@ type coord = {
   x : int;
 }
 
-module CoordMap = Map.Make (struct
-  type t = coord
-
-  let compare = compare
-end)
-
-let find map x = map |> CoordMap.filter (fun _ ch -> ch = x) |> CoordMap.choose |> fun (pos, _) -> pos
+module CharMap = Map.Make (Char)
 
 let map_of_strings s =
-  s |> List.mapi (fun y line -> List.init (String.length line) (String.get line) |> List.mapi (fun x ch -> ({ y; x }, ch))) |> List.flatten |> CoordMap.of_list
+  s |> List.mapi (fun y line -> List.init (String.length line) (String.get line) |> List.mapi (fun x ch -> (ch, { y; x }))) |> List.flatten |> CharMap.of_list
 
 let keypad = map_of_strings [ "789"; "456"; "123"; " 0A" ]
 let keypad2 = map_of_strings [ " ^A"; "<v>" ]
-let robot = find keypad 'A'
-let avoid = find keypad ' '
-let robot2 = find keypad2 'A'
-let avoid2 = find keypad2 ' '
-let human = robot2
-let code = List.hd input
-let code = List.init (String.length code) (String.get code)
 
-let rec move avoid r t =
-  if r.x < t.x && { r with x = r.x + 1 } <> avoid then '>' :: move avoid { r with x = r.x + 1 } t
-  else if r.x > t.x && { r with x = r.x - 1 } <> avoid then '<' :: move avoid { r with x = r.x - 1 } t
-  else if r.y < t.y && { r with y = r.y + 1 } <> avoid then 'v' :: move avoid { r with y = r.y + 1 } t
-  else if r.y > t.y && { r with y = r.y - 1 } <> avoid then '^' :: move avoid { r with y = r.y - 1 } t
-  else [ 'A' ]
+let move avoid r t =
+  let horizontal = if r.x < t.x then List.init (t.x - r.x) (fun _ -> '>') else if r.x > t.x then List.init (r.x - t.x) (fun _ -> '<') else [] in
+  let vertical = if r.y < t.y then List.init (t.y - r.y) (fun _ -> 'v') else if r.y > t.y then List.init (r.y - t.y) (fun _ -> '^') else [] in
+  ((if { x = r.x; y = t.y } <> avoid then [ vertical @ horizontal @ [ 'A' ] ] else [])
+  @ if { x = t.x; y = r.y } <> avoid then [ horizontal @ vertical @ [ 'A' ] ] else [])
+  |> List.sort_uniq compare
 
-let rec loop keypad avoid robot = function
-  | [] -> []
-  | hd :: tl ->
-      let next = find keypad hd in
-      let moves = move avoid robot next in
-      moves @ loop keypad avoid next tl
+let min_list = function
+  | [] -> None
+  | x :: xs -> Some (List.fold_left min x xs)
 
-let sequence = loop keypad avoid robot code
-let () = List.iter (Printf.printf "%c") sequence
-let () = Printf.printf "\n"
+let rec loop sequence = function
+  | [] -> List.length sequence
+  | keypad :: tl ->
+      let robot = CharMap.find 'A' keypad in
+      let avoid = CharMap.find ' ' keypad in
+      List.fold_left
+        (fun (robot, len) sequence ->
+          let next = CharMap.find sequence keypad in
+          let options = move avoid robot next in
+          let l = min_list (List.map (fun opt -> len + loop opt tl) options) in
+          (next, Option.value ~default:9999 l))
+        (robot, 0) sequence
+      |> fun (_, res) -> res
 
-let sequence = loop keypad2 avoid2 robot2 sequence
-let () = List.iter (Printf.printf "%c") sequence
-let () = Printf.printf "\n"
+let complexity =
+  List.fold_left
+    (fun acc code ->
+      let code_as_int = Scanf.sscanf code " %iA " @@ fun i -> i in
+      let code = List.init (String.length code) (String.get code) in
+      let sequence = loop code [ keypad; keypad2; keypad2 ] in
+      let complexity = code_as_int * sequence in
+      let () = Printf.printf "%i * %i = %i\n" sequence code_as_int (sequence * code_as_int) in
+      complexity + acc)
+    0 input
 
-let sequence = loop keypad2 avoid2 human sequence
-let () = List.iter (Printf.printf "%c") sequence
-let () = Printf.printf "\n"
-
+let () = Printf.printf "part 1: %i\n" complexity
